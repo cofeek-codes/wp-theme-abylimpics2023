1c11e0723cc497298b6728633929347e04942b6f
cofeek-codes <kormyshev11@mail.ru>
2023-03-03 12:31:51 +0500

    wp-theme

---
+++ functions.php
@@ -0,0 +1,42 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'include_styles' );
+add_action( 'wp_enqueue_scripts', 'include_js' );
+
+
+function include_styles() {
+    // echo "include style hook worked";
+    wp_enqueue_style('style', get_stylesheet_uri());
+
+    wp_enqueue_style('default', get_template_directory_uri().'/assets/css/default.css');
+    wp_enqueue_style('layout', get_template_directory_uri().'/assets/css/layout.css');
+    wp_enqueue_style('media-queries', get_template_directory_uri().'/assets/css/media-queries.css');
+
+
+}
+
+
+function include_js() {
+    // echo "include script hook worked";
+    wp_enqueue_script('init',  get_template_directory_uri().'/assets/js/init.js');
+    wp_enqueue_script('doupletap',  get_template_directory_uri().'/assets/js/doubletaptogo.js');
+    wp_enqueue_script('modernize',  get_template_directory_uri().'/assets/js/modernizr.js');
+    wp_enqueue_script('slider',  get_template_directory_uri().'/assets/js/jquery.flexslider.js');
+
+
+}
+
+
+
+// jquery
+
+
+
+add_action( 'wp_enqueue_scripts', 'jquery' );
+function jquery() {
+	// отменяем зарегистрированный jQuery
+	// вместо "jquery-core", можно вписать "jquery", тогда будет отменен еще и jquery-migrate
+	wp_deregister_script( 'jquery-core' );
+	wp_register_script( 'jquery-core', '//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js');
+	wp_enqueue_script( 'jquery' );
+}
